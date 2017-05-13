84f62eca377c97e0d744b91e2f95dd8ae38994c9
Jane Mery Miranda <janymeryc@gmail.com>
2017-05-13 20:30:58 -0300

    Second scenario is about send a message to another user, in this case, greenmile user.

--- features/sample.feature
+++ features/sample.feature
@@ -5,3 +5,10 @@ Scenario: Log in
 	And I fill a valid e-mail and password
 	When I select login button
 	Then I should see the home page
+
+Scenario: Send a message to Greenmile user
+  Given I am logged on
+  When I select message button
+  And I write a message
+  And select send
+  Then the message should be sent to Greenmile user
--- features/steps/sample_steps.rb
+++ features/steps/sample_steps.rb
@@ -17,3 +17,31 @@ Then(/^I should see the home page$/) do
   expect(page).to have_content('Teste do Twit')
   expect(page).to have_content('@JanymeryTeste')
 end
+
+Given(/^I am logged on$/) do
+  visit('/')
+  fill_in id: 'signin-email', with: 'JanymeryTeste'
+  fill_in id: 'signin-password', with: 'TestedoTwitter'
+  click_button('Log in')
+  expect(page).to have_content('Teste do Twit')
+  expect(page).to have_content('@JanymeryTeste')
+end
+
+When(/^I select message button$/) do
+  click_button('global-new-tweet-button')
+end
+
+When(/^I write a message$/) do
+  timestamp = Time.new.to_i
+  @my_tweet_message = "This is my tweet message number #{timestamp}"
+  find('#tweet-box-global').set(@my_tweet_message)
+end
+
+When(/^select send$/) do
+  find('#global-tweet-dialog-dialog > div.modal-content > div.modal-tweet-form-container > form > div.TweetBoxToolbar > div.TweetBoxToolbar-tweetButton.tweet-button > button').click
+end
+
+Then(/^the message should be sent to Greenmile user$/) do
+  save_page
+  expect(page).to have_content(@my_tweet_message)
+end
